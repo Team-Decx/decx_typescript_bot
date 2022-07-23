441fe9febbe7fb29d3934a3a0109d6e57d39cf18
iambenji3 <109809772+iambenji3@users.noreply.github.com>
2022-07-23 12:10:58 +0100

    Update messageCreate.ts

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed, Permissions } from "discord.js";
+import { Message, EmbedBuilder, Permissions } from "discord.js";
 import { Event } from "../Interfaces";
 import dotenv from 'dotenv';
 dotenv.config();
@@ -6,7 +6,7 @@ dotenv.config();
 export const event: Event = {
     name: 'messageCreate',
     run: async (client, message: Message) => {
-        if (message.channel.type ===  'DM') return;
+        if (message.channel instanceof DMChannel) return;
         if (message.author.bot || !message.content.startsWith(`${process.env.PREFIX}`)) return;
         const args = message.content.slice(1).trim().split(/ +/g);
         const cmd = args.shift()?.toLowerCase();
